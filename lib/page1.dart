f5333b84935af4ac506087131291701251f92cef
Rucha1060 <ruchabakare10@gmail.com>
2022-06-22 19:19:06 +0530

    first commit

---
+++ lib/page1.dart
@@ -0,0 +1,170 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late File _image;
+
+  @override
+  Widget build(BuildContext context) {
+
+    ImagePicker image = ImagePicker();
+late File file;
+String url="";
+getImage() async{
+   var img= await image.pickImage(source: ImageSource.camera);
+   setState(() {
+     {
+      file=File(img!.path);
+     }
+   });
+
+
+}
+
+uploadFile() async{
+  var imagefile =FirebaseStorage.instance.ref().child("path").child("/.jpg");
+  UploadTask task =imagefile.putFile(file);
+  TaskSnapshot snapshot=await task;
+
+ url= await snapshot.ref.getDownloadURL();
+ await FirebaseFirestore.instance.collection("images").doc().set({
+  "imageurl":url 
+ });
+ print(url);
+
+}
+
+
+    
+
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+              icon: Icon(FontAwesomeIcons.arrowLeft),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+          title: Text('Edit Profile'),
+        ),
+        body: Builder(
+        builder: (context) =>  Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.center,
+                    child: CircleAvatar(
+                      radius: 100,
+                      backgroundColor: Color(0xff476cfb),
+                      child: ClipOval(
+                        child: new SizedBox(
+                          width: 180.0,
+                          height: 180.0,
+                          child: (_image!=null)?Image.file(
+                            _image,
+                            fit: BoxFit.fill,
+                          ):Image.network(
+                            "https://images.unsplash.com/photo-1502164980785-f8aa41d53611?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 60.0),
+                    child: IconButton(
+                      icon: Icon(
+                        FontAwesomeIcons.camera,
+                        size: 30.0,
+                      ),
+                      onPressed: () {
+                        getImage();
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Container(
+                      child: Column(
+                        children: <Widget>[
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text('add caption',
+                                style: TextStyle(
+                                    color: Colors.blueGrey, fontSize: 18.0)),
+                          ),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text('Michelle James',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                      child: Icon(
+                        FontAwesomeIcons.pen,
+                        color: Color(0xff476cfb),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+             
+                  RaisedButton(
+                    color: Color(0xff476cfb),
+                    onPressed: () {
+                     uploadFile();
+                    },
+                                     
+                    elevation: 4.0,
+                    splashColor: Colors.blueGrey,
+                    child: Text(
+                      'Submit',
+                      style: TextStyle(color: Colors.white, fontSize: 16.0),
+                    ),
+                  ),
+              
+                ],
+              )
+            
+          ),
+        ),
+        
+        );
+  }
+}
